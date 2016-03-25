ce2730c2004806b725d8146eecf5b00723e5366d
Alex <kid-kat91@hotmail.com>
2016-03-25 03:21:14 +0200

    Update Player.cs

--- Player.cs
+++ Player.cs
@@ -1,34 +1,28 @@
-﻿using UnityEngine;
+using UnityEngine;
 using System.Collections;
 
 public class Player : MonoBehaviour {
 
     public float playerSpeed = 5.0f;
+    public Vector2 jump;
 
 	// Use this for initialization
 	void Start () {
 
         // player spawn point
 
-        transform.position = new Vector3(-100, 3, -25);
+        transform.position = new Vector3(0, 3, 0);
 
         //transform.rotation = new Quaternion(0, 180, 0, 0);
     }
 	
 	// Update is called once per frame
-	void Update () {
+	void Update (){
 
-       
-
-            transform.Translate(Vector3.left * Input.GetAxis("Horizontal") * playerSpeed * Time.deltaTime);
-
-
-       
-          
-            
-            
-        
-
-        transform.Translate(Vector3.up * Input.GetAxis("Vertical") * playerSpeed * Time.deltaTime);
+        transform.Translate(Vector3.left * Input.GetAxis("Horizontal") * playerSpeed * Time.deltaTime);
+        if (Input.GetKey("space"))
+        {
+            GetComponent<Rigidbody2D>().AddForce(jump, ForceMode2D.Impulse);
+        }
 	}
 }
